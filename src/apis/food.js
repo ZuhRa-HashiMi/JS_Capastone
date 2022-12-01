f2f4c73f75a10b0693571cc354dbf72111e5575f
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-01 21:02:03 +0430

    export pageinat function

--- src/apis/food.js
+++ src/apis/food.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable import/no-cycle */
 import paginate from '../index.js';
 
@@ -10,24 +9,11 @@ const postsPerPage = 10;
 // Get current posts
 let indexOfLastPost = currentPage * postsPerPage;
 let indexOfFirstPost = indexOfLastPost - postsPerPage;
-// const showingNumbers = document.querySelector('.showing-numbers');
-// const mealsCounter = document.getElementById('meals-counter');
+
 
 // Get length of meals
 export const getMealsLength = () => {
-  const totalMeals = meals.length;
-//   showingNumbers.innerHTML = `  <p class="text-sm text-gray-700">
-//   Showing
-//   <span class="font-medium">${indexOfFirstPost + 1}</span>
-//   to
-//   <span class="font-medium">${
-//   currentPosts.length >= 10 ? indexOfLastPost : totalMeals
-// }</span>
-//   of
-//   <span class="font-medium">${totalMeals}</span>
-//   meals
-// </p>`;
-//   mealsCounter.innerHTML = `<p class="text-sm font-bold text-gray-700">${totalMeals} meals</p>`;
+ 
 };
 
 export const getCurrentPosts = async () => {
@@ -36,13 +22,7 @@ export const getCurrentPosts = async () => {
 };
 
 export const searchFood = async (term) => {
-  const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`,
-  );
-  const data = await response.json();
-  meals = data.meals;
-  paginate(meals, currentPage);
-  return getCurrentPosts();
+
 };
 
 export const defaultFood = async () => {
@@ -56,27 +36,13 @@ export const defaultFood = async () => {
 };
 
 export const handlePrevBtn = () => {
-  if (currentPage > 1) {
-    currentPage -= 1;
-  }
-  indexOfLastPost = currentPage * postsPerPage;
-  indexOfFirstPost = indexOfLastPost - postsPerPage;
-  getCurrentPosts();
+
 };
 
 export const handleNextBtn = () => {
-  if (currentPage < Math.ceil(meals.length / postsPerPage)) {
-    currentPage += 1;
-  }
-
-  indexOfLastPost = currentPage * postsPerPage;
-  indexOfFirstPost = indexOfLastPost - postsPerPage;
-  getCurrentPosts();
+  
 };
 
 export const handlePageBtn = (page) => {
-  currentPage = page;
-  indexOfLastPost = currentPage * postsPerPage;
-  indexOfFirstPost = indexOfLastPost - postsPerPage;
-  getCurrentPosts();
+ 
 };
