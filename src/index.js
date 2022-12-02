a81edca1776a0c2ad415e44e41588989c995ef14
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-02 13:46:41 +0430

    add pagenite at the end of home page to show the pagenite

--- src/index.js
+++ src/index.js
@@ -103,6 +103,34 @@ nextBtn.forEach((btn) => {
   });
 });
 
-export default (meals) => {
+export default (meals, currentPage) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(meals.length / postsPerPage); i += 1) {
+    pageNumbers.push(i);
+  }
+  const page = document.querySelector('.page');
+  page.innerHTML = '';
+  const ul = document.createElement('ul');
+  ul.classList.add('inline-flex');
+  pageNumbers.forEach((number) => {
+    const li = document.createElement('li');
+    li.classList.add('page-item');
+    if (number === currentPage) {
+      li.classList.add('active');
+    }
+    li.innerHTML = `<a class="page-link bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium" href="#">${number}</a>`;
+    li.addEventListener('click', (e) => {
+      e.preventDefault();
+      handlePageBtn(number);
+      if (searchFood(term)) {
+        template(searchFood(term), mealsEl, resultHeading);
+      } else if (defaultFood()) {
+        defaultTemplate(defaultFood(), mealsEl, getLike(), resultHeading);
+      }
+    });
 
+    ul.appendChild(li);
+  });
+  page.appendChild(ul);
 };
+
