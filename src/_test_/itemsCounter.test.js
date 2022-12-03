9a742e6e377aa0b4bfc25c8379d027c4a291aa07
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-03 10:58:45 +0430

    add test for comment counter

--- src/_test_/itemsCounter.test.js
+++ src/_test_/itemsCounter.test.js
@@ -1,13 +1,10 @@
 import ItemsCounter from '../_mock_/itemsCounter.js';
 
 // test for pagination
-describe('return the lengty of array zuhra', () => {
-  test('return the lengty of array zuhra', () => {
-    // Arrange
-    const items = [1, 2, 3, 4, 5, 6];
-    // ACT
+describe('return the lengty of array', () => {
+  test('return the lengty of array', () => {
+    const items = [1, 2, 3, 4, 5];
     const itemsCounter = new ItemsCounter(items);
-    // Assert
-    expect(itemsCounter.getLength()).toBe(6);
+    expect(itemsCounter.getLength()).toBe(5);
   });
 });
