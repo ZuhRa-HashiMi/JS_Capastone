84d86ec5e99b27b79302e1ca82795e9607307569
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-03 12:35:30 +0430

    add aaa pattern into the unit test file

--- src/_test_/commentCounter.test.js
+++ src/_test_/commentCounter.test.js
@@ -2,9 +2,11 @@ import CommentsCounter from '../_mock_/commentsCounter.js';
 
 describe('Check Items count number', () => {
   test('return the length of array', () => {
+    // Arrange
     const comments = ['comment1', 'comment2', 'comment3', 'comment4', 'comment5', 'comment6', 'comment7', 'comment8'];
-
+    // ACT
     const commentCounter = new CommentsCounter(comments);
+    // Assert
     expect(commentCounter.getLength()).toEqual(8);
   });
 });
