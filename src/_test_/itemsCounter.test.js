623686145b73eba6d2ded1432b98a7480089c9e3
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-03 10:35:44 +0430

    add aaa pattren

--- src/_test_/itemsCounter.test.js
+++ src/_test_/itemsCounter.test.js
@@ -1,10 +1,13 @@
 import ItemsCounter from '../_mock_/itemsCounter.js';
 
 // test for pagination
-describe('return the lengty of array', () => {
-  test('return the lengty of array', () => {
-    const items = [1, 2, 3, 4, 5];
+describe('return the lengty of array zuhra', () => {
+  test('return the lengty of array zuhra', () => {
+    // Arrange
+    const items = [1, 2, 3, 4, 5, 6];
+    // ACT
     const itemsCounter = new ItemsCounter(items);
-    expect(itemsCounter.getLength()).toBe(5);
+    // Assert
+    expect(itemsCounter.getLength()).toBe(6);
   });
 });
